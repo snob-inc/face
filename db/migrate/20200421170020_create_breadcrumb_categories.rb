861e95e4cef4a012e42e5203fc11e836f8d4d355
arthur-sorami <s0rq3.vb@gmail.com>
2020-04-22 02:06:03 +0900

    [add]カテゴリ用のテーブル作成

---
+++ db/migrate/20200421170020_create_breadcrumb_categories.rb
@@ -0,0 +1,10 @@
+class CreateBreadcrumbCategories < ActiveRecord::Migration[6.0]
+  def change
+    create_table :breadcrumb_categories do |t|
+      t.string :name, null: false, unique: true
+      t.references :breadcrumb_master, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/models/breadcrumb_category_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BreadcrumbCategoryTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
