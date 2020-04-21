5e0a7689d1efc2b1689dd89a8d6c418590815835
arthur-sorami <s0rq3.vb@gmail.com>
2020-04-22 02:52:47 +0900

    [modify]enum定義

--- app/models/breadcrumb.rb
+++ app/models/breadcrumb.rb
@@ -1,4 +1,10 @@
 class Breadcrumb < ApplicationRecord
   belongs_to :breadcrumb_master
   belongs_to :user
+  enum content_status: {
+    open: 0,
+    close: 1,
+    draft: 2,
+    garbage: 3
+  }
 end
