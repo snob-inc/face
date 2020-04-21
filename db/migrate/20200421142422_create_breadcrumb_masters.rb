5b19889d0f4bb978204a0d739a1970cb3beda38e
arthur-sorami <s0rq3.vb@gmail.com>
2020-04-22 02:05:47 +0900

    [add]masterデータ用のDB作成

---
+++ db/migrate/20200421142422_create_breadcrumb_masters.rb
@@ -0,0 +1,9 @@
+class CreateBreadcrumbMasters < ActiveRecord::Migration[6.0]
+  def change
+    create_table :breadcrumb_masters do |t|
+      t.string :content, null: false, unique: true
+
+      t.timestamps
+    end
+  end
+end
