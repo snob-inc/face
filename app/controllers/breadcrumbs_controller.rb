6868fac9b7eafa2c82c65da2af797ed3c75f1371
arthur-sorami <s0rq3.vb@gmail.com>
2020-04-23 01:16:02 +0900

    [modify]rootパスの設定

--- app/controllers/breadcrumbs_controller.rb
+++ app/controllers/breadcrumbs_controller.rb
@@ -1,4 +1,4 @@
 class BreadcrumbsController < ApplicationController
-  def new
-  end
+    def new
+    end
 end
--- test/controllers/breadcrumbs_controller_test.rb
+++ test/controllers/breadcrumbs_controller_test.rb
@@ -1,9 +1,7 @@
 require 'test_helper'
 
 class BreadcrumbsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get breadcrumbs_new_url
-    assert_response :success
-  end
-
+  # test "the truth" do
+  #   assert true
+  # end
 end
