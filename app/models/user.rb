c067b23d202f7b20aee7bc7f97529863c791bc1e
arthur-sorami <s0rq3.vb@gmail.com>
2020-04-24 02:38:51 +0900

    [modify]User登録のValidation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  has_secure_password
 end
