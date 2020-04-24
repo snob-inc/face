b8aa2ab32396d286211691c23c072f8c0191013a
arthur-sorami <s0rq3.vb@gmail.com>
2020-04-25 02:28:54 +0900

    [modify]デフォルト言語の変更

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Face
   class Application < Rails::Application
     config.load_defaults 6.0
+    config.i18n.default_locale = :ja
     config.generators do |generators|
       generators.template_engine = :slim
     end
