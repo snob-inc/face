9da4ba0c878b85f67bb72754c1e8bdb2c687bbf5
arthur-sorami <s0rq3.vb@gmail.com>
2020-04-25 05:17:01 +0900

    [modify]migrationした

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_22_163753) do
+ActiveRecord::Schema.define(version: 2020_04_24_191915) do
 
   create_table "breadcrumb_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2020_04_22_163753) do
     t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
   add_foreign_key "breadcrumb_categories", "breadcrumb_masters"
