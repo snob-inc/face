87c280356d43748325c633be34fa805457dbd927
arthur-sorami <s0rq3.vb@gmail.com>
2020-04-25 05:16:25 +0900

    [add]Unique Keyにindexをはった

---
+++ db/migrate/20200424191915_add_index_user_name_and_email.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class AddIndexUserNameAndEmail < ActiveRecord::Migration[6.0]
+  def change
+    change_table :users, bulk: true do |t|
+      t.index :name, unique: true
+      t.index :email, unique: true
+    end
+  end
+end
