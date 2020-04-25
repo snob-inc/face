65963b7f4dbd967a07541aa44aad23748cb4b297
arthur-sorami <s0rq3.vb@gmail.com>
2020-04-26 01:38:20 +0900

    [modify]サインイン機能を実装した

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,20 @@
 class SessionsController < ApplicationController
   def new; end
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      flash[:notice] = "サインインしました"
+      redirect_to root_path
+    else
+      flash.now[:danger] = "サインインに失敗しました"
+      render :new
+    end
+  end
+
+  def destroy
+    signed_out
+    flash[:notice] = "サインアウトしました"
+    redirect_to root_path
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,22 @@
 module SessionsHelper
+
+  # サインイン時に取得したuser_idをsessionに入れる
+  def sign_in(user)
+    session[:user_id] = user.id
+  end
+
+  # 現在のログインユーザーを取得
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # ログイン中の場合trueを返す
+  def signed_in?
+    !current_user.nil?
+  end
+
+  def signed_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
