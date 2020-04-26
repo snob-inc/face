d43ae31c4e58070e8aafdb4d144197133bd9ed3e
arthur-sorami <s0rq3.vb@gmail.com>
2020-04-27 02:36:39 +0900

    [modify]中間テーブル作成

--- app/models/breadcrumb_category.rb
+++ app/models/breadcrumb_category.rb
@@ -1,2 +1,4 @@
 class BreadcrumbCategory < ApplicationRecord
+  has_many :breadcrumb_masters_categories
+  has_many :breadcrumb_masters, through: :breadcrumb_masters_categories
 end
--- app/models/breadcrumb_master.rb
+++ app/models/breadcrumb_master.rb
@@ -1,2 +1,4 @@
 class BreadcrumbMaster < ApplicationRecord
+  has_many :breadcrumb_masters_categories
+  has_many :breadcrumb_categories, through: :breadcrumb_masters_categories
 end
