3451dc68061aaea98a163c10919c0d38712bbb7f
arthur-sorami <s0rq3.vb@gmail.com>
2020-05-03 23:41:08 +0900

    [delete]不要Migrationファイルの削除

--- db/migrate/20200426142158_remove_breadcrumb_master_id_from_breadcrumb_categories.rb
+++
@@ -1,5 +0,0 @@
-class RemoveBreadcrumbMasterIdFromBreadcrumbCategories < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :breadcrumb_categories, :breadcrumb_master_id, :bigint
-  end
-end
